d7a65d63c77a44fb5e77a1e1ec7c1db0b9bddb54
hainuo <admin@hainuo.info>
2016-06-15 21:47:29 +0800

    修正顶部图片大小 下一步增加list按钮 点击后可以进入快捷操作页面

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React, {Component,} from 'react'
-import {View, TouchableHighlight, Text,Image, StyleSheet} from 'react-native'
+import {View, TouchableHighlight, Text, Image, StyleSheet} from 'react-native'
 
 export default class Header extends Component {
 
@@ -11,8 +11,19 @@ export default class Header extends Component {
 
     render() {
         return (
-            <View style={{flex:1,}}>
-                <Image source={{uri: 'http://120.27.108.168/demohtml/app/images/pglogo.png'}} style={{ width:100,height:60 }}/>
+            <View
+                style={{flex:1,flexDirection:'row',backgroundColor:'#FCFFFF',justifyContent:'center',alignItems:'center'}}>
+                <Image
+                    source={require( '../images/logo.png')}
+                    resizeMode="cover"
+                    style={{ margin:0,padding:0 }}
+                />
+                <View style={{flex:1}}>
+                    <Text
+                        style={{textAlign:'center',fontSize:18,fontWeight:'bold',color:'red'}}>
+                        PinGang360
+                    </Text>
+                </View>
             </View>
         );
     }
